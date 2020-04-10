3e0e233ffc15ee1b291e1022d1f74376d1590c6c
Louie <louiejf1@gmail.com>
2020-04-10 13:28:52 -0700

    4102020-128

--- readme.js
+++ readme.js
@@ -30,121 +30,51 @@ const axios = require("axios"); //https://www.npmjs.com/package/axios
 
 
 
-let tempUserName = "lhttps://api.github.com/users/louiejf1";
+let tempUserName = "https://api.github.com/users/louiejf1";
 let gitHubApi = "https://api.github.com";
 let current_user_authorizations_html_url = "https://api.github.com/hub";
 
-https://wilsonmar.github.io/github-api/
+https: //wilsonmar.github.io/github-api/
 
 
-axios.get(tempUserName)
-  .then(function(res) {
-    
-    console.log(res);
-    let name = res.data.name;
-    console.log(name);
-    let avatar = res.data.avatar_url;
-    console.log(avatar);
+    axios.get(tempUserName)
+    .then(function (res) {
+
+        //console.log(res);
+        let name = res.data.name;
+        //console.log(name);
+        let avatar = res.data.avatar_url;
+        //console.log(avatar);
+        let repos_url = res.data.repos_url;
+        //console.log(repos_url);
+
+        axios.get(repos_url)
+            .then(function (res1) {
+                //console.log(res1);
+
+                for (let i = 0; i < res1.length; i++) {
+                    // console.log(res1[i]);
+                    repoList.push(res1[i].name);
+
+                }
+                //   let repo1 = res1.data[1].name;
+                //   console.log(repo1);
+            });
+
+        repoList = [];
+        console.log(repoList);
+
+        let test = 0;
 
 
-  });
+        fs.appendFile("readme.txt", test + 1, function (err) {
 
-// console.log(promptUser.name);
+            if (err) {
+                return console.log(err);
+            }
+            ss
+            console.log("Success!");
 
-// node index.js
+        });
 
-
-
-// The README will be populated with the following:
-
-// * At least one badge
-// * Project title
-// * Description
-// * Table of Contents
-// * Installation
-// * Usage
-// * License
-// * Contributing
-// * Tests
-// * Questions
-//   * User GitHub profile picture
-//   * User GitHub email
-
-// Following the [common templates for user stories](https://en.wikipedia.org/wiki/User_story#Common_templates), we can frame this challenge as follows:
-
-// ```
-// AS A developer
-
-// I WANT a README generator
-
-// SO THAT I can easily put together a good README for a new project
-// ```
-
-// Refer to the [Good README guide](../../01-HTML-Git-CSS/04-Supplemental/Good-README-Guide/README.md).
-
-// ## Business Context
-
-// When preparing a report for stakeholders, it is important to have up-to-date information about members of the development team.
-// Rather than navigating to each team member's GitHub profile, a command-line application will allow for quick and easy generation of profiles in PDF format.
-
-// ## Minimum Requirements
-
-// * Functional, deployed application.
-
-// * GitHub repository with a unique name and a README describing project.
-
-// * The generated README includes a bio image from the user's GitHub profile.
-
-// * The generated README includes the user's email.
-
-// * The generated README includes the following sections: 
-//   * Title
-//   * Description
-//   * Table of Contents
-//   * Installation
-//   * Usage
-//   * License
-//   * Contributing
-//   * Tests
-//   * Questions
-
-// * The generated README includes 1 badge that's specific to the repository.
-
-// ```
-// GIVEN the developer has a GitHub profile and a repository
-
-// WHEN prompted for the developer's GitHub username and repo specific information
-
-// THEN a README for the repo is generated
-// ```
-// - - -
-
-// ## Commit Early and Often
-
-// One of the most important skills to master as a web developer is version control. Building the habit of committing via Git is important for two reasons:
-
-// * Your commit history is a signal to employers that you are actively working on projects and learning new skills.
-
-// * Your commit history allows you to revert your code base in the event that you need to return to a previous state.
-
-// Follow these guidelines for committing:
-
-// * Make single-purpose commits for related changes to ensure a clean, manageable history. If you are fixing two issues, make two commits.
-
-// * Write descriptive, meaningful commit messages so that you and anyone else looking at your repository can easily understand its history.
-
-// * Don't commit half-done work, for the sake of your collaborators (and your future self!).
-
-// * Test your application before you commit to ensure functionality at every step in the development process.
-
-// We would like you to have more than 200 commits by graduation, so commit early and often!
-
-// ## Submission on BCS
-
-// You are required to submit the following:
-
-// * An animated GIF demonstrating the app functionality
-
-// * A generated PDF of your GitHub profile
-
-// * The URL of the GitHub repository
+    });
